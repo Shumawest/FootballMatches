1efa88b7272f51f0fa8cc1f60a7a46622d7cf1d2
Shumawest <shuma-g@ukr.net>
2014-07-07 22:00:50 +0300

    Create router.js

---
+++ js/router.js
@@ -0,0 +1,15 @@
+Matches.Router.map(function() {
+  this.resource("matches", { path: '/' });
+  this.resource("match", { path: "/match/:match_id" });
+  this.resource("addComment", { path: "/match/:match_id/create" });
+});
+Matches.MatchesRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('match');
+  }
+});
+Matches.MatchRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('match', params.match_id);
+  },
+});
